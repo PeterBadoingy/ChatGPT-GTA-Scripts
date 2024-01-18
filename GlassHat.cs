31664d8eadb763a2f58b37b0be7a0ddabb757d1c
Peter Badoingy <54063403+PeterBadoingy@users.noreply.github.com>
2024-01-18 09:47:40 +0000

    Update GlassHat.cs

--- GlassHat.cs
+++ GlassHat.cs
@@ -5,10 +5,19 @@ using System.Windows.Forms;
 
 public class HatGlassesScript : Script
 {
-    int currentHatIndex = -1;
-    int currentGlassesIndex = -1;
-    int currentHatTexture = -1;
-    int currentGlassesTexture = -1;
+    bool hatsOn = false;
+    int currentHats = -1;
+    int myHats = -1;
+    int sgTexture = -1; // Hat texture index
+    bool hatsSet = false;
+    bool noHats = false;
+
+    bool glassesOn = false;
+    int currentGlasses = -1;
+    int myGlasses = -1;
+    int sgTextureGlasses = -1; // Glasses texture index
+    bool glassesSet = false;
+    bool noGlasses = false;
 
     public HatGlassesScript()
     {
@@ -36,58 +45,110 @@ public class HatGlassesScript : Script
     private void HatsEventHandler()
     {
         Ped player = Game.Player.Character;
+        currentHats = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 0);
 
-        if (currentHatIndex == -1)
+        if (currentHats == -1 && !hatsSet)
         {
-            // Player is removing the hat
-            currentHatIndex = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 0);
-            currentHatTexture = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 0);
+            noHats = true;
+            hatsSet = false;
+        }
+        else if (currentHats != -1 && !hatsSet)
+        {
+            myHats = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 0);
+            sgTexture = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 0);
+            noHats = false;
+            hatsSet = true;
+            hatsOn = true;
+        }
+        else if (currentHats == -1 && hatsSet)
+        {
+            hatsOn = false;
+        }
+        else if (hatsSet && currentHats != -1 && myHats != currentHats)
+        {
+            myHats = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 0);
+            sgTexture = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 0);
+            hatsSet = true;
+            noHats = false;
+            hatsOn = true;
+        }
 
-            PlayAnimation(player, "missheist_agency2ahelmet", "take_off_helmet_stand");
-            Wait(500); // Adjust the time as needed
-
-            // Add logic to take off the hat (if needed)
-            Function.Call(Hash.CLEAR_PED_PROP, player, 0);
-            ShowNotification("Hat removed");
+        // Takes hat off / Puts hat On
+        if (!noHats)
+        {
+            hatsOn = !hatsOn;
+            PlayAnimation(player, hatsOn ? "missheistdockssetup1hardhat@" : "missheist_agency2ahelmet", hatsOn ? "put_on_hat" : "take_off_helmet_stand");
+            if (hatsOn)
+            {
+                Wait(1200); // Adjust the time as needed
+                Function.Call(Hash.SET_PED_PROP_INDEX, player, 0, myHats, sgTexture, 2);
+                ShowNotification("Hat is on");
+            }
+            else
+            {
+                Wait(500); // Adjust the time as needed
+                Function.Call(Hash.CLEAR_PED_PROP, player, 0);
+                ShowNotification("Hat is off");
+            }
         }
         else
         {
-            // Player is putting the hat back on
-            PlayAnimation(player, "missheistdockssetup1hardhat@", "put_on_hat");
-            Wait(1200); // Adjust the time as needed
-
-            Function.Call(Hash.SET_PED_PROP_INDEX, player, 0, currentHatIndex, currentHatTexture, 2);
-            ShowNotification("Hat put back on");
-            currentHatIndex = -1;
+            ShowNotification("You are not wearing a Hat");
         }
     }
 
     private void SunglassesEventHandler()
     {
         Ped player = Game.Player.Character;
+        currentGlasses = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 1);
 
-        if (currentGlassesIndex == -1)
+        if (currentGlasses == -1 && !glassesSet)
         {
-            // Player is removing the glasses
-            currentGlassesIndex = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 1);
-            currentGlassesTexture = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 1);
+            noGlasses = true;
+            glassesSet = false;
+        }
+        else if (currentGlasses != -1 && !glassesSet)
+        {
+            myGlasses = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 1);
+            sgTextureGlasses = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 1);
+            noGlasses = false;
+            glassesSet = true;
+            glassesOn = true;
+        }
+        else if (currentGlasses == -1 && glassesSet)
+        {
+            glassesOn = false;
+        }
+        else if (glassesSet && currentGlasses != -1 && myGlasses != currentGlasses)
+        {
+            myGlasses = Function.Call<int>(Hash.GET_PED_PROP_INDEX, player, 1);
+            sgTextureGlasses = Function.Call<int>(Hash.GET_PED_PROP_TEXTURE_INDEX, player, 1);
+            glassesSet = true;
+            noGlasses = false;
+            glassesOn = true;
+        }
 
-            PlayAnimation(player, "clothingspecs", "take_off");
-            Wait(1000); // Adjust the time as needed
-
-            // Add logic to take off the glasses (if needed)
-            Function.Call(Hash.CLEAR_PED_PROP, player, 1);
-            ShowNotification("Glasses removed");
+        // Takes Glasses off / Puts Glasses On
+        if (!noGlasses)
+        {
+            glassesOn = !glassesOn;
+            PlayAnimation(player, glassesOn ? "clothingspecs" : "clothingspecs", glassesOn ? "put_on" : "take_off");
+            if (glassesOn)
+            {
+                Wait(3500); // Adjust the time as needed
+                Function.Call(Hash.SET_PED_PROP_INDEX, player, 1, myGlasses, sgTextureGlasses, 2);
+                ShowNotification("Glasses are on");
+            }
+            else
+            {
+                Wait(1000); // Adjust the time as needed
+                Function.Call(Hash.CLEAR_PED_PROP, player, 1);
+                ShowNotification("Glasses are off");
+            }
         }
         else
         {
-            // Player is putting the glasses back on
-            PlayAnimation(player, "clothingspecs", "put_on");
-            Wait(3500); // Adjust the time as needed
-
-            Function.Call(Hash.SET_PED_PROP_INDEX, player, 1, currentGlassesIndex, currentGlassesTexture, 2);
-            ShowNotification("Glasses put back on");
-            currentGlassesIndex = -1;
+            ShowNotification("You are not wearing Glasses");
         }
     }
 
